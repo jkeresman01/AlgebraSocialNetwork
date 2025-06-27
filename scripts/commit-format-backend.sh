1343983380e4cf0ac00196706f9b934b65d3ac82
Josip <josipkeresman@gmail.com>
2025-06-27 16:18:02 +0200

    Allow commits from github actions

--- scripts/commit-format-backend.sh
+++ scripts/commit-format-backend.sh
@@ -3,10 +3,9 @@ set -e
 
 git config --global user.name "github-actions[bot]"
 git config --global user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
 git add .
-if ! git diff --cached --quiet; then
-  git commit -m "Fix formatting nonsense"
-  git push
-fi
+git diff --cached --quiet || git commit -m "chore: auto-format backend via Spotless"
+git push
 
